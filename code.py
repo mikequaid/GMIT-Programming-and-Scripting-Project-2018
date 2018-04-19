5d55bd2039c054860e539447bbe001efc95be3d6
Mike Quaid <36292540+mikequaid@users.noreply.github.com>
2018-04-19 17:52:59 +0100

    Update code.py

--- code.py
+++ code.py
@@ -9,27 +9,27 @@ data=numpy.genfromtxt('iris.txt',delimiter=',') # read data file into an array
 # the first row is [0]
 
 # calculate the mean of each column in the array
-meanfirstcol=numpy.mean(data[:,0]) 
+meansepallength=numpy.mean(data[:,0]) 
 print(meanfirstcol)
 
-meansecondcol=numpy.mean(data[:,1]) 
+meansepalwidth=numpy.mean(data[:,1]) 
 print(meansecondcol)
 
-meanthirdcol=numpy.mean(data[:,2]) 
+meanpetallength=numpy.mean(data[:,2]) 
 print(meanthirdcol)
 
-meanfourthcol=numpy.mean(data[:,3]) 
+meanpetalwidth=numpy.mean(data[:,3]) 
 print(meanfourthcol)
 
 # calculate the maximum value in each column in the array
-maxfirstcol=numpy.max(data[:,0])
+maxsepallength=numpy.max(data[:,0])
 print(maxfirstcol)
 
-maxsecondcol=numpy.max(data[:,1])
+maxsepalwidth=numpy.max(data[:,1])
 print(maxsecondcol)
 
-maxthirdcol=numpy.max(data[:,2])
+maxpetallength=numpy.max(data[:,2])
 print(maxthirdcol)
 
-maxfourthcol=numpy.max(data[:,3])
+maxpetalwidth=numpy.max(data[:,3])
 print(maxfourthcol)
